fd13b533c906b823b3375a4adb2e14be4efa9652
AbdulArif <abdularif.dgp@gmail.com>
2023-10-20 21:28:50 +0530

    update

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -2,22 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SettingScreen extends StatelessWidget {
+  const SettingScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
+      padding: const EdgeInsets.only(top: 40, left: 20, right: 20),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
+          const Text(
             "Settings",
             style: TextStyle(
-              fontSize: 30,
+              fontSize: 25,
               fontWeight: FontWeight.w500,
             ),
           ),
-          SizedBox(height: 30),
-          ListTile(
+          const SizedBox(height: 20),
+          const ListTile(
             leading: CircleAvatar(
               radius: 30,
               backgroundImage: AssetImage("images/doctor1.jpg"),
@@ -26,147 +28,147 @@ class SettingScreen extends StatelessWidget {
               "Dear Programmer",
               style: TextStyle(
                 fontWeight: FontWeight.w500,
-                fontSize: 25,
+                fontSize: 20,
               ),
             ),
             subtitle: Text("Profile"),
           ),
-          Divider(height: 50),
+          const Divider(height: 30),
           ListTile(
             onTap: () {},
             leading: Container(
-              padding: EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
               decoration: BoxDecoration(
                 color: Colors.blue.shade100,
                 shape: BoxShape.circle,
               ),
-              child: Icon(
+              child: const Icon(
                 CupertinoIcons.person,
                 color: Colors.blue,
                 size: 35,
               ),
             ),
-            title: Text(
+            title: const Text(
               "Profile",
               style: TextStyle(
                 fontWeight: FontWeight.w500,
-                fontSize: 20,
+                fontSize: 18,
               ),
             ),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 10),
           ListTile(
             onTap: () {},
             leading: Container(
-              padding: EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
               decoration: BoxDecoration(
                 color: Colors.deepPurple.shade100,
                 shape: BoxShape.circle,
               ),
-              child: Icon(
+              child: const Icon(
                 Icons.notifications_none_outlined,
                 color: Colors.deepPurple,
                 size: 35,
               ),
             ),
-            title: Text(
+            title: const Text(
               "Notifications",
               style: TextStyle(
                 fontWeight: FontWeight.w500,
                 fontSize: 20,
               ),
             ),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 10),
           ListTile(
             onTap: () {},
             leading: Container(
-              padding: EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
               decoration: BoxDecoration(
                 color: Colors.indigo.shade100,
                 shape: BoxShape.circle,
               ),
-              child: Icon(
+              child: const Icon(
                 Icons.privacy_tip_outlined,
                 color: Colors.indigo,
                 size: 35,
               ),
             ),
-            title: Text(
+            title: const Text(
               "Privacy",
               style: TextStyle(
                 fontWeight: FontWeight.w500,
                 fontSize: 20,
               ),
             ),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 10),
           ListTile(
             onTap: () {},
             leading: Container(
-              padding: EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
               decoration: BoxDecoration(
                 color: Colors.green.shade100,
                 shape: BoxShape.circle,
               ),
-              child: Icon(
+              child: const Icon(
                 Icons.settings_suggest_outlined,
                 color: Colors.green,
                 size: 35,
               ),
             ),
-            title: Text(
+            title: const Text(
               "General",
               style: TextStyle(
                 fontWeight: FontWeight.w500,
                 fontSize: 20,
               ),
             ),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
           ),
-          SizedBox(height: 20),
+          const SizedBox(height: 10),
           ListTile(
             onTap: () {},
             leading: Container(
-              padding: EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
               decoration: BoxDecoration(
                 color: Colors.orange.shade100,
                 shape: BoxShape.circle,
               ),
-              child: Icon(
+              child: const Icon(
                 Icons.info_outline_rounded,
                 color: Colors.orange,
                 size: 35,
               ),
             ),
-            title: Text(
+            title: const Text(
               "About Us",
               style: TextStyle(
                 fontWeight: FontWeight.w500,
                 fontSize: 20,
               ),
             ),
-            trailing: Icon(Icons.arrow_forward_ios_rounded),
+            trailing: const Icon(Icons.arrow_forward_ios_rounded),
           ),
-          Divider(height: 40),
+          const Divider(height: 10),
           ListTile(
             onTap: () {},
             leading: Container(
-              padding: EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
               decoration: BoxDecoration(
                 color: Colors.redAccent.shade100,
                 shape: BoxShape.circle,
               ),
-              child: Icon(
+              child: const Icon(
                 Icons.info_outline_rounded,
                 color: Colors.redAccent,
                 size: 35,
               ),
             ),
-            title: Text(
+            title: const Text(
               "Log Out",
               style: TextStyle(
                 fontWeight: FontWeight.w500,
