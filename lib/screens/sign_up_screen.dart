36cb970bbf43f629538a12ecc41ee7d07815bebc
AbdulArif <abdularif.dgp@gmail.com>
2023-10-29 18:31:10 +0530

    update

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -14,136 +14,146 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   @override
   Widget build(BuildContext context) {
-    double screenHeight = MediaQuery.of(context).size.height;
-    double imageHeight = screenHeight * 0.2; // Adjust the percentage as needed
-
     return Material(
       color: Colors.white,
-      child: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(5),
-                child: Image.asset(
-                  "images/doctors.png",
-                  height: imageHeight,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth < 600) {
+            // For smaller screens (e.g., phones)
+            return _buildContent(isDesktop: false);
+          } else {
+            // For larger screens (e.g., desktops)
+            return _buildContent(isDesktop: true);
+          }
+        },
+      ),
+    );
+  }
+
+  Widget _buildContent({required bool isDesktop}) {
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Image.asset(
+                "images/doctors.png",
+                height: isDesktop ? 200 : 100, // Adjust the image height
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 2, horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Full Name",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person),
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 2, horizontal: 15),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Full Name",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.person),
-                  ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 2, horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Email Address",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 2, horizontal: 15),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Email Address",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.email),
-                  ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 2, horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Phone Number",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.phone),
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 2, horizontal: 15),
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelText: "Phone Number",
-                    border: OutlineInputBorder(),
-                    prefixIcon: Icon(Icons.phone),
-                  ),
-                ),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 2, horizontal: 15),
-                child: TextField(
-                  obscureText: passToggle ? true : false,
-                  decoration: InputDecoration(
-                    border: const OutlineInputBorder(),
-                    label: const Text("Enter Password"),
-                    prefixIcon: const Icon(Icons.lock),
-                    suffixIcon: InkWell(
-                      onTap: () {
-                        if (passToggle == true) {
-                          passToggle = false;
-                        } else {
-                          passToggle = true;
-                        }
-                        setState(() {});
-                      },
-                      child: passToggle
-                          ? const Icon(CupertinoIcons.eye_slash_fill)
-                          : const Icon(CupertinoIcons.eye_fill),
-                    ),
-                  ),
-                ),
-              ),
-              InkWell(
-                onTap: () {},
-                child: Container(
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                  width: 335,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF7165D6),
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      const BoxShadow(
-                        color: Colors.black12,
-                        blurRadius: 4,
-                        spreadRadius: 2,
-                      ),
-                    ],
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "Create Account",
-                      style: TextStyle(
-                        fontSize: 22,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Already have an account?",
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => LoginScreen(),
-                          ));
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 15),
+              child: TextField(
+                obscureText: passToggle ? true : false,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(),
+                  label: const Text("Enter Password"),
+                  prefixIcon: const Icon(Icons.lock),
+                  suffixIcon: InkWell(
+                    onTap: () {
+                      if (passToggle == true) {
+                        passToggle = false;
+                      } else {
+                        passToggle = true;
+                      }
+                      setState(() {});
                     },
-                    child: const Text(
-                      "Log In",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF7165D6),
-                      ),
+                    child: passToggle
+                        ? const Icon(CupertinoIcons.eye_slash_fill)
+                        : const Icon(CupertinoIcons.eye_fill),
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 15),
+                width: isDesktop ? 400 : 335, // Adjust the button width
+                decoration: BoxDecoration(
+                  color: const Color(0xFF7165D6),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 4,
+                      spreadRadius: 2,
+                    ),
+                  ],
+                ),
+                child: const Center(
+                  child: Text(
+                    "Create Account",
+                    style: TextStyle(
+                      fontSize: 22,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
-                ],
+                ),
               ),
-            ],
-          ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Already have an account?",
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginScreen(),
+                        ));
+                  },
+                  child: const Text(
+                    "Log In",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF7165D6),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
