37a974bd816ce8dd80499a5cf31e464a9b0ce242
AbdulArif <abdularif.dgp@gmail.com>
2023-10-29 12:26:54 +0530

    primaryColor

--- lib/constant/color.dart
+++ lib/constant/color.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primaryColor = const Color(0xff2A7FBA),
+  static Color primaryColor = const Color(0xff91B4DF),
       yollowColor = const Color(0xffFF9F00),
       bgColor = const Color(0xffF5F5F5),
       textColor = Color.fromARGB(255, 0, 0, 0),
