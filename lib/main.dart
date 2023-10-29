4c478efd26ccb3cee40d0f80ef6af0fde7f4410f
AbdulArif <abdularif.dgp@gmail.com>
2023-10-29 18:23:37 +0530

    up

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:healthcare/screens/login_screen.dart';
+import 'package:healthcare/screens/sign_up_screen.dart';
 import 'package:healthcare/screens/welcome_screen.dart';
 
 void main() {
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
+      home: SignUpScreen(),
     );
   }
 }
